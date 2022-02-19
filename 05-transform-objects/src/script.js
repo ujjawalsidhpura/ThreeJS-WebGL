f654f6dc2d80f3bb4a89e226fe5a9aad005712b8
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-19 12:36:38 -0500

    scale rotate postion

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -8,12 +8,45 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+//POSITION
+// mesh.position.x = -1
+// mesh.position.y = -1
+// mesh.position.z = -1
+// //OR in one go
+// // mesh.position.set(1,1,1)
+
+//SCALE
+// mesh.scale.x = 2
+// mesh.scale.y = 1
+// mesh.scale.z = 0.5
+
+//ROTATION
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+mesh.position.x = 0.7
+mesh.position.y = - 0.6
+mesh.position.z = 1
+mesh.scale.x = 2
+mesh.scale.y = 0.25
+mesh.scale.z = 0.5
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
 scene.add(mesh)
+console.log(mesh.position.length())
 
 /**
  * Sizes
@@ -28,7 +61,9 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
+
 
 /**
  * Renderer
