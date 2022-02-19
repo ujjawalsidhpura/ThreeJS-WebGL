7d1f7c60a3cb41123f39c723d4273cadddafb0c2
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-19 13:02:11 -0500

    tick

---
+++ 06-animations/src/script.js
@@ -0,0 +1,57 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+/**
+ * Animate
+ */
+let time = Date.now()
+
+const tick = () => {
+
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+    // Update objects
+    mesh.rotation.y += 0.01 * deltaTime
+    // mesh.rotation.z += 0.01 * deltaTime (+ fasters, - slower)
+    // mesh.rotation.x += 0.01 * deltaTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
