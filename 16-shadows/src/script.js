e8a5aa6c01093cae21eca6d3671b56062cba5a84
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-20 11:31:57 -0500

    alternate more efficient shadow tech

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -16,6 +16,12 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+
+/**
  * Lights
  */
 // Ambient light
@@ -71,8 +77,7 @@ pointLight.castShadow = true
 pointLight.position.set(- 1, 1, 0)
 scene.add(pointLight)
 
-const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
-scene.add(pointLightCameraHelper)
+
 pointLight.shadow.mapSize.width = 1024
 pointLight.shadow.mapSize.height = 1024
 
@@ -87,6 +92,7 @@ material.roughness = 0.7
 gui.add(material, 'metalness').min(0).max(1).step(0.001)
 gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
+
 /**
  * Objects
  */
@@ -96,15 +102,36 @@ const sphere = new THREE.Mesh(
 )
 sphere.castShadow = true
 
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(5, 5),
+//     new THREE.MeshBasicMaterial({
+//         map: bakedShadow
+//     })
+// )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphere, sphereShadow, plane)
 
 /**
  * Sizes
@@ -150,7 +177,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.shadowMap.enabled = true
+
+renderer.shadowMap.enabled = false
 
 /**
  * Animate
@@ -159,6 +187,16 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update the sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    // Update the shadow
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3
 
     // Update controls
     controls.update()
