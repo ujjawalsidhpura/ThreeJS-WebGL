6913a580cc4b478fe819428d1a4e6ab26b482ec5
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-20 12:04:49 -0500

    add random graves

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -25,42 +25,99 @@ const textureLoader = new THREE.TextureLoader()
  */
 
 // House container
-const house = new THREE.Group()
-scene.add(house)
+{
+    const house = new THREE.Group()
+    scene.add(house)
 
-const walls = new THREE.Mesh(
-    new THREE.BoxGeometry(4, 2.5, 4),
-    new THREE.MeshStandardMaterial({ color: '#ac8e82' })
-)
-walls.position.y = 1.25
-house.add(walls)
+    const walls = new THREE.Mesh(
+        new THREE.BoxGeometry(4, 2.5, 4),
+        new THREE.MeshStandardMaterial({ color: '#ac8e82' })
+    )
+    walls.position.y = 1.25
+    house.add(walls)
 
-// Floor
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({ color: '#a9c388' })
-)
-floor.rotation.x = - Math.PI * 0.5
-floor.position.y = 0
-scene.add(floor)
+    // Floor
+    const floor = new THREE.Mesh(
+        new THREE.PlaneGeometry(20, 20),
+        new THREE.MeshStandardMaterial({ color: '#a9c388' })
+    )
+    floor.rotation.x = - Math.PI * 0.5
+    floor.position.y = 0
+    scene.add(floor)
 
-// Roof
-const roof = new THREE.Mesh(
-    new THREE.ConeGeometry(3.5, 1, 4),
-    new THREE.MeshStandardMaterial({ color: '#b35f45' })
-)
-roof.rotation.y = Math.PI * 0.25
-roof.position.y = 2.5 + .5
-house.add(roof)
+    // Roof
+    const roof = new THREE.Mesh(
+        new THREE.ConeGeometry(3.5, 1, 4),
+        new THREE.MeshStandardMaterial({ color: '#b35f45' })
+    )
+    roof.rotation.y = Math.PI * 0.25
+    roof.position.y = 2.5 + .5
+    house.add(roof)
 
-// Door
-const door = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
-)
-door.position.y = 1
-door.position.z = 2 + 0.01
-house.add(door)
+    // Door
+    const door = new THREE.Mesh(
+        new THREE.PlaneGeometry(2, 2),
+        new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+    )
+    door.position.y = 1
+    door.position.z = 2 + 0.01
+    house.add(door)
+
+    // Bushes
+    const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+    const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+
+    const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush1.scale.set(0.5, 0.5, 0.5)
+    bush1.position.set(0.8, 0.2, 2.2)
+
+    const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush2.scale.set(0.25, 0.25, 0.25)
+    bush2.position.set(1.4, 0.1, 2.1)
+
+    const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush3.scale.set(0.4, 0.4, 0.4)
+    bush3.position.set(- 0.8, 0.1, 2.2)
+
+    const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush4.scale.set(0.15, 0.15, 0.15)
+    bush4.position.set(- 1, 0.05, 2.6)
+
+    house.add(bush1, bush2, bush3, bush4)
+}
+
+// Graves
+{
+    const graves = new THREE.Group()
+    scene.add(graves)
+
+    const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+    const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' })
+
+    //     We are going to create a random angle on a circle. Remember that a full revolution is 2 times π. Then we are going to use that angle on both a sin(...) and a cos(...). This is how you position things on a circle when you have the angle. And finally we also multiply those sin(...) and cos(...) results by a random value because we don't want the graves to be positioned on a perfect circle.
+    // 
+    for (let i = 0; i < 50; i++) {
+        const angle = Math.random() * Math.PI * 2 // Random angle
+        const radius = 3 + Math.random() * 6      // Random radius
+        const x = Math.cos(angle) * radius        // Get the x position using cosinus
+        const z = Math.sin(angle) * radius        // Get the z position using sinus
+
+        // Create the mesh
+        const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+
+        // Position
+        grave.position.set(x, 0.3, z)
+
+        // Rotation
+        grave.rotation.z = (Math.random() - 0.5) * 0.4
+        grave.rotation.y = (Math.random() - 0.5) * 0.4
+
+        // Add to the graves container
+        graves.add(grave)
+    }
+
+}
+
 
 /**
  * Lights
