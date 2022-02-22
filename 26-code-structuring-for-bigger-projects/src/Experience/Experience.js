6cbef902c336ac04823b07de0a318d56907966c3
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-22 11:11:20 -0500

    resouces class and class environment

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -3,6 +3,8 @@ import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
 import Camera from './Camera.js'
 import Renderer from './Renderer.js'
+import World from './World/World.js'
+import Resources from './Utils/Resources.js'
 
 export default class Experience {
 
@@ -17,8 +19,10 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources()
         this.camera = new Camera(this)
         this.renderer = new Renderer()
+        this.world = new World()
 
         // Resize event
         this.sizes.on('resize', () => {
