92eadb29fd0505682bf53bcaf34b23a14f59fff1
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-28 10:34:54 -0500

    tet

---
+++ 22-physics/src/test.js
@@ -0,0 +1,22 @@
+// window.addEventListener('scroll', () => {
+//     // ...
+
+//     if (newSection != currentSection) {
+//         // ...
+
+//         gsap.to(
+//             sectionMeshes[currentSection].rotation,
+//             {
+//                 duration: 1.5,
+//                 ease: 'power2.inOut',
+//                 x: '+=6',
+//                 y: '+=3'
+//             }
+//         )
+//     }
+// })
+
+// const renderer = new THREE.WebGLRenderer({
+//     canvas: canvas,
+//     alpha: true
+// })
